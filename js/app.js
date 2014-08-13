07804c19769ff217fc2c6a6f251a8e82f1c2c6ce
Andrea Moretti <axyzxp@gmail.com>
2014-08-13 19:08:13 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,77 @@
-$(document).foundation();
+$(document).foundation({
+  orbit: {
+    slide_number: false,
+    navigation_arrows: false,
+    resume_on_mouseout: true
+  }
+});
+
+$(document).ready(function(){
+  $('.clients').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    responsive: [
+      {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+    ]
+  });
+
+  $('.portfolio').slick({
+    slide: 'img',
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    responsive: [
+      {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
+  });
+
+});
