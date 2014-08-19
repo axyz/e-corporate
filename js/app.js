dc93790b839fc34165957f623db6606534d54b97
Andrea Moretti <axyzxp@gmail.com>
2014-08-19 16:44:43 +0200

    fixed lazyload + masonry behaviour

--- js/app.js
+++ js/app.js
@@ -74,13 +74,24 @@ $(document).ready(function(){
     ]
   });
 
-  var $masonry = $('#masonry');
-  $("img.lazy").lazyload();
-
-  $masonry.imagesLoaded(function(){
-    $masonry.masonry({
-      columnWidth: 400
-    });
-  });
 
 });
+
+window.onload = function () {
+  var $masonry = $('#masonry');
+    $("img.lazy").lazyload({
+      appear: function() {
+          $masonry.masonry();
+      }
+    }
+    );
+
+    $masonry.imagesLoaded(function(){
+      $masonry.masonry({
+        isFitWidth: true,
+        columnWidth: 410
+      });
+    });
+
+    $masonry.masonry();
+}
